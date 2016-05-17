5f93b4442841284f12b7cabfeda6f9c2ebdc603c
Syahrul Aiman Shaharuddin <syahrul.aiman@my.ibm.com>
2016-05-17 11:48:34 +0800

    typo

--- lib/binaries_cfcli.sh
+++ lib/binaries_cfcli.sh
@@ -2,7 +2,7 @@ install_cfcli() {
   local dir="$1"
   CF_VERSION=6.17
 
-  local download_url="https://cli.run.pivotal.io/stable?release=linux64-binary&version=CF_VERSION.0&source=github-rel"
+  local download_url="https://cli.run.pivotal.io/stable?release=linux64-binary&version=$CF_VERSION.0&source=github-rel"
   echo "Downloading CF CLI [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/cf.tar.gz || (echo "Unable to download cf CLI; does it exist?" && false)
   echo "Download complete!"
